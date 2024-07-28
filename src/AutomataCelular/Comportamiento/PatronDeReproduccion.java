0c51069860c56e587139b1439a01ed1d1d27d94a
CaroRob30 <caroshotokan@hotmail.com>
2024-07-28 16:06:34 -0600

    Avance 17/07

--- src/AutomataCelular/Comportamiento/PatronDeReproduccion.java
+++ src/AutomataCelular/Comportamiento/PatronDeReproduccion.java
@@ -1,54 +1,22 @@
 package AutomataCelular.Comportamiento;
 
-import AutomataCelular.Estadisticas.RegistroDeEventos;
-import AutomataCelular.SeresVivos.Crias.Cria;
-import AutomataCelular.UbicacionSeresVivos.Celda;
-import AutomataCelular.Configuracion;
-import AutomataCelular.UbicacionSeresVivos.DetectorDeVecinos;
-import AutomataCelular.SeresVivos.*;
-import AutomataCelular.SeresVivos.Crias.GatoCria;
-import AutomataCelular.SeresVivos.Crias.PerroCria;
-import AutomataCelular.SeresVivos.Crias.RatonCria;
-import AutomataCelular.Tablero;
-import AutomataCelular.UbicacionSeresVivos.RegistrarUbicacion;
-
-import java.util.List;
-import java.util.Optional;
+import AutomataCelular.SeresVivos.Animal;
+import AutomataCelular.SeresVivos.SerVivo;
+/*
+Esta clase encapula la lógica de la reproducción de los seres vivos en la simulación delegando la lógica a la
+clase 'Reproductor' que se encarga de la reproducción de los seres vivos. Lo cual facilita la extensión y
+modificación de las reglas de reproducción sin afectar al programa.
+ */
 
 public class PatronDeReproduccion implements IPatronDeComportamiento {
-    private RegistroDeEventos registroDeEventos;
-    private Tablero tablero;
+    private final Reproductor reproductor;
 
     public PatronDeReproduccion() {
-        tablero = Tablero.getInstancia();
-        registroDeEventos = RegistroDeEventos.getInstancia();
+        reproductor = new Reproductor();
     }
 
     @Override
-    public void ejecutar(Animal animal, SerVivo serVivo) {
-        if (animal.sonDeLaMismaEspecie(serVivo) && animal.estanEnEdadReproductiva((Animal) serVivo)) {
-            List<int[]> vecinos = DetectorDeVecinos.detectarVecinos(
-                    Tablero.obtenerPosicion(animal)[0],
-                    Tablero.obtenerPosicion(animal)[1],
-                    Configuracion.FILAS,
-                    Configuracion.COLUMNAS);
-            for (int[] posicion : vecinos) {
-                Celda celdaVecina = tablero.getCelda(posicion[0], posicion[1]);
-                if (celdaVecina.estaVacia()) {
-                    Animal cria = animal.clonar((Animal) serVivo);
-                    if (cria != null) {
-                        celdaVecina.agregarSerVivo(cria);
-                        RegistrarUbicacion.establecerUbicacion(cria, celdaVecina);
-                        // RegistroDeEventos registroDeEventos = null; // Eliminar esta línea
-                        registroDeEventos.registrarNacimiento(serVivo.getClass().getSimpleName(), 1,
-                                RegistrarUbicacion.obtenerUbicacion(animal));
-                        System.out.println("Se reprodujeron: " + animal.getClass().getSimpleName());
-                    }
-                    animal.consumirEnergiaPorReproduccion();
-                    ((Animal) serVivo).consumirEnergiaPorReproduccion();
-                    return;
-                }
-            }
-        }
+    public void iniciar(Animal animal, SerVivo serVivo) {
+        reproductor.reproducir(animal, serVivo);
     }
 }
