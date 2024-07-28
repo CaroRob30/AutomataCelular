4cf79606d47cd05479e0f16a80e026fd3f800af6
CaroRob30 <caroshotokan@hotmail.com>
2024-07-28 15:49:08 -0600

    Avance 17/07

--- src/AutomataCelular/Comportamiento/PatronDeAlimentacion.java
+++ src/AutomataCelular/Comportamiento/PatronDeAlimentacion.java
@@ -2,15 +2,25 @@ package AutomataCelular.Comportamiento;
 
 import AutomataCelular.Configuracion;
 import AutomataCelular.SeresVivos.Animal;
-import AutomataCelular.SeresVivos.Planta;
 import AutomataCelular.SeresVivos.SerVivo;
+/*
+Esta clase implementa la interfaz 'IPatronDeComportamiento' y define el método 'iniciar' que se encarga de
+incremetar la energía del animal que se alimenta de la planta y disminuir la energía de la planta, hasta un
+máximo de energía permitida para el animal. De igual manera, la energía de la planta disminuye por la cantidad
+predeterminada en la configuración de la simulación.
 
+ */
 public class PatronDeAlimentacion implements IPatronDeComportamiento {
     @Override
-    public void ejecutar(Animal animal, SerVivo serVivo) {
-        if (serVivo instanceof Planta) {
-            animal.setEnergia(animal.getEnergia() + Configuracion.ENERGIA_ABSORBIDA_DE_PLANTA);
-            serVivo.setEnergia(serVivo.getEnergia() - Configuracion.ENERGIA_ABSORBIDA_DE_PLANTA);
-        }
+    public void iniciar(Animal animal, SerVivo serVivo) {
+
+            int energiaPotencial = animal.getEnergia() + Configuracion.ENERGIA_CONSUMIDA_DE_LA_PLANTA;
+
+            animal.setEnergia(Math.min(energiaPotencial, Configuracion.ENERGIA_MAX_ANIMAL));
+
+            serVivo.setEnergia(serVivo.getEnergia() - Configuracion.ENERGIA_CONSUMIDA_DE_LA_PLANTA);
+
+            System.out.println("Comio: "  + serVivo.getClass().getSimpleName() + " n°: "+ serVivo.getId());
+
     }
 }
