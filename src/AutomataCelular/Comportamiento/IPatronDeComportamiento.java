499f611937d0040ffb0dedfb1b2c90f5893435c2
CaroRob30 <caroshotokan@hotmail.com>
2024-07-28 15:44:53 -0600

    Avance 17/07

--- src/AutomataCelular/Comportamiento/IPatronDeComportamiento.java
+++ src/AutomataCelular/Comportamiento/IPatronDeComportamiento.java
@@ -3,6 +3,14 @@ package AutomataCelular.Comportamiento;
 import AutomataCelular.SeresVivos.Animal;
 import AutomataCelular.SeresVivos.SerVivo;
 
+/*
+Esta interfaz define el método 'iniciar' que debe ser implementado por cualquier clase que represente un patrón
+de comportamiento en la simulación.
+Permite la creación de diferentes patrones de comportamiento para los seres vivos, como la reproducción,
+alimentación o la comunicación, que facilita la modificación y extensión de las reglas de interacción
+entre los seres vivos sin afectar al programa.
+ */
+
 public interface IPatronDeComportamiento {
-    void ejecutar(Animal animal, SerVivo serVivo);
+    void iniciar(Animal animal, SerVivo serVivo);
 }
