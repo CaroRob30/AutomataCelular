895d52e761a8e8341f5ee3a8e3354df8ed37d28b
CaroRob30 <caroshotokan@hotmail.com>
2024-07-28 16:41:04 -0600

    Avance 17/07

---
+++ src/AutomataCelular/Comportamiento/FiltroDeReproduccion.java
@@ -0,0 +1,36 @@
+package AutomataCelular.Comportamiento;
+
+import AutomataCelular.SeresVivos.Animal;
+import AutomataCelular.SeresVivos.SerVivo;
+
+import java.util.HashSet;
+import java.util.Set;
+/*
+Esta clase se encarga de filtrar las parejas que se pueden reproducir, así como el hecho de ser de la misma especie,
+estar en edad reproductiva y tener la energía necesaria para reproducirse.
+De igual manera, se encarga de llevar un registro de las parejas que ya se han reproducido para evitar que se
+reproduzcan nuevamente.
+ */
+
+public class FiltroDeReproduccion {
+    private final static Set<String> parejasReproducidas = new HashSet<>();
+
+    public boolean sePuedenReproducir(Animal animal, SerVivo serVivo) {
+        String parejaID = generarParejaID(animal, serVivo);
+        return !parejasReproducidas.contains(parejaID) &&
+                animal.sonDeLaMismaEspecie(serVivo) &&
+                animal.estanEnEdadReproductiva((Animal) serVivo) &&
+                animal.tieneEnergiaReproductiva() &&
+                ((Animal) serVivo).tieneEnergiaReproductiva();
+    }
+
+    private String generarParejaID(SerVivo serVivo1, SerVivo serVivo2) {
+        long id1 = serVivo1.getId();
+        long id2 = serVivo2.getId();
+        return id1 < id2 ? id1 + "-" + id2 : id2 + "-" + id1;
+    }
+
+    public static void limpiarParejasReproducidas() {
+        parejasReproducidas.clear();
+    }
+}
