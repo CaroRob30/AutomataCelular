7a7cb80af573560658887e604a3e2969701c1964
CaroRob30 <caroshotokan@hotmail.com>
2024-07-28 20:02:59 -0600

    Avance 17/07

--- src/AutomataCelular/Movimiento/RegistrarUbicacion.java
+++ src/AutomataCelular/Movimiento/RegistrarUbicacion.java
@@ -1,11 +1,17 @@
-package AutomataCelular.UbicacionSeresVivos;
+package AutomataCelular.Movimiento;
 
-import AutomataCelular.Configuracion;
 import AutomataCelular.SeresVivos.SerVivo;
-import AutomataCelular.Tablero;
+import AutomataCelular.UbicacionSeresVivos.Celda;
 
 import java.util.HashMap;
 import java.util.Map;
+/*
+Gestiona la ubicación y posición de los seres vivos en el tablero.
+Utiliza dos mapas: uno para asociar cada ser vivo con una celda en específico, y el otro para registrar las
+coordenadas de cada ser vivo.
+tiene métodos para establecer y obtener la ubicación y posición de un ser vivo.
+Esta clase tiene como objetivo facilitar el seguimiento y la actualización de las posiciones de los seres vivos.
+ */
 
 public class RegistrarUbicacion {
     private static final Map<SerVivo, Celda> ubicaciones = new HashMap<>();
@@ -17,10 +23,6 @@ public class RegistrarUbicacion {
 
     public static Celda obtenerUbicacion(SerVivo serVivo) {
         return ubicaciones.get(serVivo);
-    }
-
-    public static void eliminarUbicacion(SerVivo serVivo) {
-        ubicaciones.remove(serVivo);
     }
 
     public static void establecerPosicion(SerVivo serVivo, int fila, int columna) {
