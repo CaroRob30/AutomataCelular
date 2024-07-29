ee082f7fef7ccec564c2b0817228f24780b82bad
CaroRob30 <caroshotokan@hotmail.com>
2024-07-28 19:26:06 -0600

    Avance 17/07

---
+++ src/AutomataCelular/Estadisticas/GeneradorCSV/GeneradorCSV.java
@@ -0,0 +1,30 @@
+package AutomataCelular.Estadisticas.GeneradorCSV;
+
+import AutomataCelular.Configuracion;
+
+import java.io.PrintWriter;
+/*
+Esta clase abstracta se facilita la generación de archivos CSV con información proporcionada.
+Define dos métodos abstractos que deben ser implementados por las clases que hereden de esta.
+El método 'getNombreArchivo' debe retornar el nombre del archivo CSV que se desea generar.
+El método 'escribirContenido' debe escribir el contenido del archivo CSV.
+El método 'generarCSV' se encarga de crear el archivo CSV en la ruta especificada en 'Configuracion'.
+ */
+
+public abstract class GeneradorCSV {
+
+    protected abstract String getNombreArchivo();
+
+    protected abstract void escribirContenido(PrintWriter contenido);
+
+    public void generarCSV() {
+        String rutaArchivo = Configuracion.RUTA_DE_ARCHIVO;
+        try (PrintWriter writer = new PrintWriter(rutaArchivo + "\\" + getNombreArchivo())) {
+            escribirContenido(writer);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
