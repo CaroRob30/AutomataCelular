3986ef5f4988e390bfb101927b46aaa261976e76
CaroRob30 <caroshotokan@hotmail.com>
2024-07-28 19:32:29 -0600

    Avance 17/07

---
+++ src/AutomataCelular/Estadisticas/GeneradorCSV/GeneradorDeMovimientosCSV.java
@@ -0,0 +1,26 @@
+package AutomataCelular.Estadisticas.GeneradorCSV;
+import AutomataCelular.Estadisticas.RegistroDeMovimientos;
+
+import java.io.PrintWriter;
+import java.util.List;
+/*
+Esta clase extiende también de la clase abstracta 'GeneradorCSV'.
+Sobreescribe los métodos 'getNombreArchivo' y 'escribirContenido'
+para generar un archivo CSV con los movimientos registrados.
+ */
+public class GeneradorDeMovimientosCSV extends GeneradorCSV {
+
+    @Override
+    protected String getNombreArchivo() {
+        return "registroDeMovimientos.csv";
+    }
+
+    @Override
+    protected void escribirContenido(PrintWriter contenido) {
+        contenido.println("Evento; Especie; ID; Año; De [F, C]; A [F, C]");
+        List<String> movimientos = RegistroDeMovimientos.getMovimientos();
+        for (String movimiento : movimientos) {
+            contenido.println(movimiento);
+        }
+    }
+}
