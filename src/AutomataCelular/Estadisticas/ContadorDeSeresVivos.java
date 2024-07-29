7ae4976c194c69314064c5bbe5ecdfb6a38184fe
CaroRob30 <caroshotokan@hotmail.com>
2024-07-28 18:38:44 -0600

    Avance 17/07

---
+++ src/AutomataCelular/Estadisticas/ContadorDeSeresVivos.java
@@ -0,0 +1,20 @@
+package AutomataCelular.Estadisticas;
+/*
+Esta clase se encarga de calcular y devolver el total de seres vivos de una especie en específico
+y el total de seres vivos en la simulación.
+'getTotalEspecie' devuelve el total de seres vivos de una especie en específico.
+'getTotalDeSeresVivos' devuelve el total de seres vivos en la simulación.
+ */
+public class ContadorDeSeresVivos {
+    public static int getTotalEspecie(String especie) {
+        return Contador.getContadorDeSeresVivos().getOrDefault(especie, 0);
+    }
+
+    public static int getTotalDeSeresVivos() {
+        int total = 0;
+        for (int count : Contador.getContadorDeSeresVivos().values()) {
+            total += count;
+        }
+        return total;
+    }
+}
