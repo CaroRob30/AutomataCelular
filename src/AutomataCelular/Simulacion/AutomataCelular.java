9c38e6c0128d2bb70aa95bde08099efa895bf2a8
CaroRob30 <caroshotokan@hotmail.com>
2024-07-28 21:15:57 -0600

    Avance 17/07

--- src/AutomataCelular/Simulacion/AutomataCelular.java
+++ src/AutomataCelular/Simulacion/AutomataCelular.java
@@ -1,23 +1,15 @@
 package AutomataCelular.Simulacion;
 
-import AutomataCelular.Comportamiento.ConvivenciaAnimal;
-import AutomataCelular.Movimiento.MovimientoAnimal;
-import AutomataCelular.SeresVivos.*;
-
-import AutomataCelular.Tablero;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-
+/*
+Esta clase sirve como el controlador principal de la simulación. Inicializa el objeto GeneradorDeSimulacion y
+proporciona el método iniciar() para comenzar la simulación y la ejecuta durante un número de iteraciones.
+ */
 public class AutomataCelular {
-    private final Tablero tablero = Tablero.getInstancia();
-    private final ExecutorService executor;
+
     private final GeneradorDeSimulacion generadorDeSimulacion;
 
     public AutomataCelular() {
-        this.executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
         this.generadorDeSimulacion = new GeneradorDeSimulacion();
     }
 
