b3d014fe20cf8a6ca6b4b3cfe1bd5fb8fe9c09f6
CaroRob30 <caroshotokan@hotmail.com>
2024-07-28 20:07:42 -0600

    Avance 17/07

--- src/AutomataCelular/SeresVivos/Animal.java
+++ src/AutomataCelular/SeresVivos/Animal.java
@@ -4,11 +4,21 @@ import AutomataCelular.Configuracion;
 import AutomataCelular.SeresVivos.Crias.GatoCria;
 import AutomataCelular.SeresVivos.Crias.PerroCria;
 import AutomataCelular.SeresVivos.Crias.RatonCria;
-
+/*
+Esta clase abstracta 'Animal' define el comportamiento y las características comunes de todos los animales.
+Hereda de la clase 'SerVivo' y agrega  propiedades específicas de los animales como la edad.
+Se define la energía inciial y la edad inicial de los animales.
+Proporciona métodos para simular el paso del tiempo, verificar si el animal está vivo, gestionar aspectos
+relacionados con la reproducción y la interacción con otros animales.
+También incluye un método para clonar un animal y crear una nueva cría, simulando la reproducción.
+ */
 public abstract class Animal extends SerVivo {
-    protected int energiaComida;
     protected int edad;
-    protected int edadMaxima;
+
+    public Animal() {
+        this.energia = Configuracion.ENERGIA_INICIAL_ANIMAL;
+        this.edad = Configuracion.EDAD_INICIAL_ANIMAL;
+    }
 
     public Animal clonar(Animal animal) {
         if (animal instanceof Gato) {
@@ -21,49 +31,39 @@ public abstract class Animal extends SerVivo {
         }
     }
 
-    public Animal() {
-            this.energia = Configuracion.ENERGIA_INICIAL_ANIMAL;
-            this.edad = Configuracion.EDAD_INICIAL_ANIMAL;
-            this.edadMaxima = Configuracion.EDAD_MAX_ANIMAL;
-            this.energiaComida = Configuracion.ENERGIA_COMIDA;
-        }
-
-        @Override
-        public void pasoDelTiempo () {
-            edad++;
-            energia--;
-        }
-
-
-        @Override
-        public boolean estaVivo () {
-            return edad <= edadMaxima && energia > 0;
-        }
-
-        public boolean estanEnEdadReproductiva (Animal animal){
-            return animal.getEdad() >= Configuracion.EDAD_REPRODUCTIVA
-                    && this.getEdad() >= Configuracion.EDAD_REPRODUCTIVA;
-        }
-        public boolean tieneEnergiaReproductiva () {
-            return this.getEnergia() > Configuracion.ENERGIA_REPRODUCTIVA;
-        }
-        public void consumirEnergiaPorReproduccion () {
-            this.setEnergia(this.getEnergia() - Configuracion.ENERGIA_REPRODUCTIVA);
-        }
-
-        public void consumirEnergiaPorInteraccion () {
-            this.setEnergia(this.getEnergia() - Configuracion.ENERGIA_INTERACCION_ANIMAL);
-        }
-
-        public int getEdad () {
-            return edad;
-        }
-
-        public void setEdad ( int edad){
-            this.edad = edad;
-        }
-
-        public int getEdadMaxima () {
-            return edadMaxima;
-        }
+    @Override
+    public void pasoDelTiempo() {
+        edad++;
+        energia--;
     }
+
+    @Override
+    public boolean estaVivo() {
+        return edad < Configuracion.EDAD_MAX_ANIMAL && energia > 0;
+    }
+
+    public boolean estanEnEdadReproductiva(Animal animal) {
+        return animal.getEdad() >= Configuracion.EDAD_PARA_PODER_REPRODUCIRSE
+                && this.getEdad() >= Configuracion.EDAD_PARA_PODER_REPRODUCIRSE;
+    }
+
+    public boolean tieneEnergiaReproductiva() {
+        return this.getEnergia() > Configuracion.ENERGIA_CONSUMIDA_POR_REPRODUCCION;
+    }
+
+    public void consumirEnergiaPorReproduccion() {
+        this.setEnergia(this.getEnergia() - Configuracion.ENERGIA_CONSUMIDA_POR_REPRODUCCION);
+    }
+
+    public void consumirEnergiaPorInteraccion() {
+        this.setEnergia(this.getEnergia() - Configuracion.ENERGIA_CONSUMIDA_POR_INTERACTUAR_CON_ANIMALES);
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
